3687ff1271686fbea47811df990b47006e5b7f8f
Brihi <brihi16142@iiitd.ac.in>
2019-10-26 16:43:24 -0700

    Added content in API

--- app/flask_test.py
+++ app/flask_test.py
@@ -1,27 +1,38 @@
 from flask import Flask, request, Response
 import os
 from werkzeug import utils
+from aws_detect import detect_labels_local_file
+from food2fork import get_recipes
+
 
 app = Flask(__name__)
 
+label_list = []
 
-@app.route('/ingredient', methods=['GET', 'POST'])
-def test():
+
+@app.route('/ingredient', methods=['POST'])
+def ingredient():
     image_file = request.files['image']
     print("\nReceived image File name : " + image_file.filename)
     max = get_latest_file_id()
     filename = utils.secure_filename(str(max + 1))
     image_file.save("images/{}.jpg".format(filename))
+    label = detect_labels_local_file("images/{}.jpg".format(filename))
+    if label != None:
+        label_list.append(label)
     # clear_files()
 
     return Response(response=None, status=200, mimetype="application/json")
 
 
 @app.route('/recipe', methods=['POST'])
-def end_sequence():
+def recipe(): 
     # TODO make request to recipes here
     # TODO send JSON response to client
-    return Response(response=None, status=200, mimetype="application/json")
+    response = get_recipes(label_list)
+    label_list=[]
+
+    return Response(response=response, status=200, mimetype="application/json")
 
 
 def clear_files():
--- app/food2fork.py
+++ app/food2fork.py
@@ -2,7 +2,18 @@ import requests
 
 key = "cd2098965f07ca87a249d52b79be617c"
 
+def get_recipes(list_ingredients):
+	query_ingredients = "%20".join(list_ingredients)
+	query = "https://www.food2fork.com/api/search?key="+key+"&q="+query_ingredients
+	response = requests.get(query)
+	return response.json()['recipes'][:5]
 
-response = requests.get("https://www.food2fork.com/api/search?key="+key+"&q=mango%20kiwi")
 
-print(response.json())
+def main():
+	list_ingredients =['garlic','tomato']
+
+	print(get_recipes(list_ingredients))
+
+if __name__ == "__main__":
+    main()
+
